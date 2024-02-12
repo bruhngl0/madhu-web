736465a2ebe7adacc7e2dfa757eb2655aa5b7526
bruh.ngl0 <adityashrm500@gmail.com>
2024-02-12 14:31:31 +0530

    ll

--- src/components/Ani.jsx
+++ src/components/Ani.jsx
@@ -17,7 +17,7 @@ const Ani = () => {
             <div className="video-section">
          
                 <video controls>
-                    <source src="Eureka.mp4" type="video/mp4" />
+                    <source src="Eureka01.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
